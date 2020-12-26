cc47d0561781b4879574f580c6ae6050a48702ce
Maxi <maximiliano.cespedes@coolblue.nl>
2020-12-26 11:20:15 +0100

    fix build?

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Auth } from "aws-amplify";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
 
 import Layout from "../Layout";
 import { Link } from "react-router-dom";
@@ -12,6 +13,8 @@ const ErrorMessage = styled.div`
   color: red;
 `;
 const Login = () => {
+  const history = useHistory();
+
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const { register, handleSubmit } = useForm();
@@ -21,7 +24,7 @@ const Login = () => {
 
     try {
       const user = await Auth.signIn(username, password);
-      console.log(user);
+      history.push("/");
     } catch (error) {
       setError(error);
     }
